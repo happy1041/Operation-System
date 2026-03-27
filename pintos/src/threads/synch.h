0e9c7dd5b525968c88c31ee7a81a9800ba0e085c
Hasinon <qq1041637640@163.com>
2026-03-27 20:43:33 +0800

    Lab1 2.1

--- pintos/src/threads/synch.h
+++ pintos/src/threads/synch.h
@@ -3,6 +3,7 @@
 
 #include <list.h>
 #include <stdbool.h>
+#include <list.h>
 
 /** A counting semaphore. */
 struct semaphore 
---
+++ pintos/start_pintos.sh
@@ -0,0 +1,7 @@
+#\!/bin/bash
+# 一键启动 Pintos 脚本
+
+cd src/threads
+make
+cd build
+pintos --
